352a1bd22a02729a9832fd098f7e2d4f409a4b54
EduGollner0609 <dudugollner@gmail.com>
2025-06-26 03:50:21 -0300

    Criando projeto Spring.

---
+++ src/main/java/com/eduardo/user_post_mongo/UserPostMongoApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.user_post_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPostMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPostMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/user_post_mongo/UserPostMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.user_post_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPostMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
