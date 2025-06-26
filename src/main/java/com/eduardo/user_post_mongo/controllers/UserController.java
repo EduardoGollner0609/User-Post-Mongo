a89a1f289fb8a3d50f526cba4f2889c532b09d67
EduGollner0609 <dudugollner@gmail.com>
2025-06-26 04:49:23 -0300

    FindById de user.

--- src/main/java/com/eduardo/user_post_mongo/controllers/UserController.java
+++ src/main/java/com/eduardo/user_post_mongo/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.eduardo.user_post_mongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> findAll() {
         List<UserDTO> users = service.findAll();
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        UserDTO user = service.findById(id);
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/eduardo/user_post_mongo/services/UserService.java
+++ src/main/java/com/eduardo/user_post_mongo/services/UserService.java
@@ -3,6 +3,7 @@ package com.eduardo.user_post_mongo.services;
 import com.eduardo.user_post_mongo.models.dtos.UserDTO;
 import com.eduardo.user_post_mongo.models.entities.User;
 import com.eduardo.user_post_mongo.repositories.UserRepository;
+import com.eduardo.user_post_mongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +18,13 @@ public class UserService {
     public List<UserDTO> findAll() {
         List<User> users = repository.findAll();
         return users.stream().map(UserDTO::new).toList();
+    }
+
+    public UserDTO findById(String id) {
+        User user = repository
+                .findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(String.format("Usuário do id %d não foi encontrado!", id)));
+        return new UserDTO(user);
     }
 }
